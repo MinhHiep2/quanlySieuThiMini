a3c972349bd6921d47df61fafb7168626667fbf0
MinhHiep2 <hiep29042021@gmail.com>
2025-03-20 20:07:00 +0700

    Update

---
+++ ManagementSupermarket/ManagementSupermarket/frmLogin.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ManagementSupermarket
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Error = new System.Windows.Forms.Label();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnHidePassWord = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // lbl_Error
+            // 
+            this.lbl_Error.AutoSize = true;
+            this.lbl_Error.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.lbl_Error.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Error.Location = new System.Drawing.Point(484, 293);
+            this.lbl_Error.Name = "lbl_Error";
+            this.lbl_Error.Size = new System.Drawing.Size(52, 24);
+            this.lbl_Error.TabIndex = 15;
+            this.lbl_Error.Text = "Error";
+            this.lbl_Error.Visible = false;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.btn_Exit.ForeColor = System.Drawing.Color.Black;
+            this.btn_Exit.Location = new System.Drawing.Point(688, 330);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(137, 42);
+            this.btn_Exit.TabIndex = 13;
+            this.btn_Exit.Text = "Thoát";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.btn_Login.ForeColor = System.Drawing.Color.Black;
+            this.btn_Login.Location = new System.Drawing.Point(488, 330);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(147, 42);
+            this.btn_Login.TabIndex = 14;
+            this.btn_Login.Text = "Đăng nhập";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Password.Location = new System.Drawing.Point(584, 226);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(209, 38);
+            this.txt_Password.TabIndex = 12;
+            this.txt_Password.UseSystemPasswordChar = true;
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Username.Location = new System.Drawing.Point(584, 163);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(261, 38);
+            this.txt_Username.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(459, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(445, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tài Khoản:";
+            // 
+            // btnHidePassWord
+            // 
+            this.btnHidePassWord.IconChar = FontAwesome.Sharp.IconChar.Eye;
+            this.btnHidePassWord.IconColor = System.Drawing.Color.Black;
+            this.btnHidePassWord.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnHidePassWord.IconSize = 35;
+            this.btnHidePassWord.Location = new System.Drawing.Point(799, 222);
+            this.btnHidePassWord.Name = "btnHidePassWord";
+            this.btnHidePassWord.Size = new System.Drawing.Size(46, 38);
+            this.btnHidePassWord.TabIndex = 16;
+            this.btnHidePassWord.UseVisualStyleBackColor = true;
+            this.btnHidePassWord.Click += new System.EventHandler(this.btnHidePassWord_Click_1);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::ManagementSupermarket.Properties.Resources.SIÊU_THỊ_MINI;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(900, 479);
+            this.Controls.Add(this.btnHidePassWord);
+            this.Controls.Add(this.lbl_Error);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_Username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.DoubleBuffered = true;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLogin";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmLogin_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_Error;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private FontAwesome.Sharp.IconButton btnHidePassWord;
+    }
+}
---
+++ ManagementSupermarket/ManagementSupermarket/frmLogin.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagementSupermarket
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnHidePassWord_Click(object sender, EventArgs e)
+        {
+            if (btnHidePassWord.IconChar == FontAwesome.Sharp.IconChar.Eye)
+            {
+                txt_Password.UseSystemPasswordChar = false;
+                btnHidePassWord.IconChar = FontAwesome.Sharp.IconChar.EyeSlash;
+            }
+            else if (btnHidePassWord.IconChar == FontAwesome.Sharp.IconChar.EyeSlash)
+            {
+                txt_Password.UseSystemPasswordChar = true;
+                btnHidePassWord.IconChar = FontAwesome.Sharp.IconChar.Eye;
+            }
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            if(txt_Username.Text == "admin" && txt_Password.Text == "admin")
+            {
+                frmLogin frmMain = new frmLogin();
+                frmMain.Show();
+                this.Hide();
+                MessageBox.Show("Bạn đã đăng nhập thành công!", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnHidePassWord_Click_1(object sender, EventArgs e)
+        {
+            if (btnHidePassWord.IconChar == FontAwesome.Sharp.IconChar.Eye)
+            {
+                txt_Password.UseSystemPasswordChar = false;
+                btnHidePassWord.IconChar = FontAwesome.Sharp.IconChar.EyeSlash;
+            }
+            else if (btnHidePassWord.IconChar == FontAwesome.Sharp.IconChar.EyeSlash)
+            {
+                txt_Password.UseSystemPasswordChar = true;
+                btnHidePassWord.IconChar = FontAwesome.Sharp.IconChar.Eye;
+            }
+        }
+    }
+}
